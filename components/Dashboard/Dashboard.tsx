58570039148dd33e3f4f7be518acdc5c61c5861c
Tanishq Sharma <tanishqsh@gmail.com>
2024-06-25 21:50:03 -0400

    Removed duration selector from Impact followers

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -34,11 +34,13 @@ export default function Dashboard() {
 					<EngagementChartComponent />
 				</div>
 				<div className="flex flex-col md:flex-row justify-between space-y-4 md:space-y-0 md:space-x-4  px-4 md:px-0">
-					<div className="w-full md:w-2/3">
+					<div className="w-full md:w-2/4">
 						<TopCasts />
 					</div>
-					<div className="w-full md:w-1/3 space-y-4">
+					<div className="w-full md:w-1/4">
 						<ImpactFollowers />
+					</div>
+					<div className="w-full md:w-1/4">
 						<ImpactUnfollowers />
 					</div>
 				</div>
